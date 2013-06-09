6ada2083aa81b7b179604c881f57533a52d4911e
John Farrier <john.farrier@digitalinblue.com>
2013-06-09 13:25:19 -0400

    Added a new example.
    This new example is for a micro-optimization question posted to Stack
    Overflow.

---
+++ src/DemoComparison.cpp
@@ -0,0 +1,94 @@
+#include <random>
+
+#include <celero/Celero.h>
+
+///
+/// A second demo that was created for a Stack Overflow question:
+///
+/// http://stackoverflow.com/questions/15770049/micro-optimizing-a-c-comparison-function/
+///
+/// The code here has been updated from the original post to utilize std::random for better sampling.
+///
+class StackOverflowFixture : public celero::TestFixture
+{
+	public:
+		StackOverflowFixture() : 
+			Gen(RandomDevice()),
+			RandomBool(0.5),
+			RandomInt(-100, 100)
+		{
+		}
+
+		inline bool NoOp(bool, int, int) 
+		{
+			return true;
+		}
+
+		inline bool Compare(bool greater, int p1, int p2) 
+		{
+			if(greater == true)
+			{
+				return (p1 >= p2);
+			}
+			
+			return (p1 <= p2);
+		}
+
+		// Code for Compare 2
+		inline bool Compare2(bool greater, int p1, int p2)
+		{
+			bool ret[2] = {p1 <= p2, p1 >= p2};
+			return ret[greater];
+		}
+		// END 
+
+		// Code for Compare 3
+		inline bool Compare3(bool greater, int p1, int p2) 
+		{
+			return (!greater != !(p1 <= p2)) | (p1 == p2);
+		}
+		// END
+
+		inline bool Compare4(bool greater, int p1, int p2) 
+		{
+		  return (greater ^ (p1 <= p2)) | (p1 == p2);
+		}
+
+		// Improved with Bernoulli distribution of booleans.
+		std::random_device RandomDevice;
+	    std::mt19937 Gen;
+
+		// give "true" 1/2 of the time
+		std::bernoulli_distribution RandomBool;
+		std::uniform_int_distribution<int> RandomInt;
+};
+
+BASELINE_F(StackOverflow, Baseline, StackOverflowFixture, 100, 5000000)
+{
+	celero::DoNotOptimizeAway(NoOp(
+		this->RandomBool(this->Gen), this->RandomInt(this->Gen), this->RandomInt(this->Gen)));
+}
+
+BENCHMARK_F(StackOverflow, Compare, StackOverflowFixture, 100, 5000000)
+{
+	celero::DoNotOptimizeAway(Compare(
+		this->RandomBool(this->Gen), this->RandomInt(this->Gen), this->RandomInt(this->Gen)));
+}
+
+BENCHMARK_F(StackOverflow, Compare2, StackOverflowFixture, 100, 5000000)
+{
+	celero::DoNotOptimizeAway(Compare2(
+		this->RandomBool(this->Gen), this->RandomInt(this->Gen), this->RandomInt(this->Gen)));
+}
+
+BENCHMARK_F(StackOverflow, Compare3, StackOverflowFixture, 100, 5000000)
+{
+	celero::DoNotOptimizeAway(Compare3(
+		this->RandomBool(this->Gen), this->RandomInt(this->Gen), this->RandomInt(this->Gen)));
+}
+
+BENCHMARK_F(StackOverflow, Compare4, StackOverflowFixture, 100, 5000000)
+{
+	celero::DoNotOptimizeAway(Compare4(
+		this->RandomBool(this->Gen), this->RandomInt(this->Gen), this->RandomInt(this->Gen)));
+}
