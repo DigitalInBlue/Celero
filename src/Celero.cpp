32a54a9959f98a1a1fcba2f294883147f7252687
Nacho Valladares <nacho.valladares@pknoa.com>
2017-07-06 13:59:06 +0200

    Forcing the output file (if any) to close when finishing the bencnhmarks

--- src/Celero.cpp
+++ src/Celero.cpp
@@ -139,6 +139,7 @@ void celero::Run(int argc, char** argv)
 	}
 
 	// Has a result output file been specified?
+    bool must_close_file = false;
 	auto argument = args.get<std::string>("outputTable");
 	if(argument.empty() == false)
 	{
@@ -146,6 +147,7 @@ void celero::Run(int argc, char** argv)
 		celero::ResultTable::Instance().setFileName(argument);
 
 		celero::AddExperimentResultCompleteFunction([](std::shared_ptr<celero::Result> p) { celero::ResultTable::Instance().add(p); });
+        must_close_file = true;
 	}
 
 	// Has a result output file been specified?
@@ -185,6 +187,9 @@ void celero::Run(int argc, char** argv)
 		executor::RunAll();
 	}
 
+    if (must_close_file) {
+        celero::ResultTable::Instance().closeFile();
+    }
 	// Final output.
 	std::cout << "Complete.\n";
 }
