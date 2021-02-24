f6ccc0930ae14b80ed0cf86d24d949e243fbca20
John Farrier <john.farrier@digitalinblue.com>
2021-02-23 20:53:40 -0500

    Copyright Update 2021

--- include/celero/ExperimentResult.h
+++ include/celero/ExperimentResult.h
@@ -4,7 +4,7 @@
 ///
 /// \author	John Farrier
 ///
-/// \copyright Copyright 2015, 2016, 2017, 2018. 2019 John Farrier
+/// \copyright Copyright 2015-2021 John Farrier
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
@@ -35,12 +35,12 @@ namespace celero
 	///
 	/// \author	John Farrier
 	///
-	class CELERO_EXPORT ExperimentResult final
+	class CELERO_EXPORT ExperimentResult
 	{
 	public:
 		explicit ExperimentResult(Experiment* x);
 
-		~ExperimentResult() = default;
+		~ExperimentResult();
 
 		///
 		/// Gets a pointer to the owning experiment.
